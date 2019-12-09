834eeb08eaa9eaf2d004491b2434a6a3666818fa
Ubuntu <ubuntu@ip-172-31-25-191.us-west-2.compute.internal>
2019-12-09 09:29:26 +0000

    added index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const keys = require('./keys')
+const keys = require('./keys');
+const redis =   require ('require');
+
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    host: keys.redisPort,
+    retry_strategy: () =>
+})
+const sub = redisClient.duplicate();
+
+function fib(index) {
+    if (index < 2) return 1;
+    return fib(index - 1) + fib(index - 2);
+}

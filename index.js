c3c04b08785b7c77204005a8b5bad79410736d1e
Ubuntu <ubuntu@ip-172-31-25-191.us-west-2.compute.internal>
2019-12-09 09:33:24 +0000

    added index.js part II

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const redis =   require ('require');
 const redisClient = redis.createClient({
     host: keys.redisHost,
     host: keys.redisPort,
-    retry_strategy: () =>
+    retry_strategy: () => 1000
 })
 const sub = redisClient.duplicate();
 
@@ -12,3 +12,8 @@ function fib(index) {
     if (index < 2) return 1;
     return fib(index - 1) + fib(index - 2);
 }
+
+sub.on('message', (channel, message) => {
+    redisClient.hset('values', message, fib(parseInt(message)));
+});
+sub.subscribe('insert');
